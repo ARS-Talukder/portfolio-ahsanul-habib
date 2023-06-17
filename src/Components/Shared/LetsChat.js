d0f9ac078d3483bae0da9ce32598e486b7281806
ARS Talukder <talukderars@gmail.com>
2023-06-17 15:06:49 +0600

    design almost done for the first revision

--- src/Components/Shared/LetsChat.js
+++ src/Components/Shared/LetsChat.js
@@ -3,6 +3,7 @@ import ReactWhatsapp from 'react-whatsapp';
 
 const LetsChat = () => {
     return (
+        // ---------------WhatsApp Chat--------------------
         <div className='flex justify-end mb-1 sticky bottom-0 right-0'>
             <ReactWhatsapp className='border border-4 border-green-400 rounded-3xl hover:border-green-500' number="+880 1751 514293" message="Hello, Ahsanul Habib. I want to get in touch with you." style={{ backgroundColor: "#e0edce" }}>
                 <img src="https://i.ibb.co/p67tz8w/Chat.png" width={80} alt="chat" />
